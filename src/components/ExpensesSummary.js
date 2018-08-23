d5eef7db6ba1bebd04c6975d33c5f459716976ca
Regina Mc Guire <rmcguire@antelecom.net>
2018-08-23 05:23:12 -0700

    Refactor Expenses Summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,17 +3,14 @@ import { connect } from 'react-redux';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
-export class ExpensesSummary extends React.Component {
 
-  render() {
-    return (
-      <div>
-        {this.props.expenses &&
-          <p>Viewing {this.props.expenses.length}{this.props.expenses.length === 1 ? ` expense` : ` expenses`} totaling   {numeral(this.props.expensesTotal / 100).format('$0,0.00')}</p>}
-      </div>
-    )
-  };
-};
+export const ExpensesSummary = (props) => (
+  <div>
+    {props.expenses &&
+      <p>Viewing {props.expenses.length}{props.expenses.length === 1 ? ` expense` : ` expenses`} totaling   {numeral(props.expensesTotal / 100).format('$0,0.00')}</p>}
+  </div>
+
+)
 const mapStoreToProps = (state) => {
   return {
     expenses: selectExpenses(state.expenses, state.filters),
