58ea7af33cd615a612464fe9f8241b9a53c9a59f
Regina Mc Guire <rmcguire@antelecom.net>
2018-09-06 08:57:53 -0700

    Add private routes

---
+++ src/components/HelpPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HelpPage = () => (
+  <div>
+    Help page - <Link to="/">Go home</Link>
+  </div>
+);
+export default HelpPage;
